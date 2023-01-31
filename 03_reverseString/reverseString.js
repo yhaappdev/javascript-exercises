bf96c9fbc559500b96e7c33d4df11b691f471141
yhaappdev <121143197+yhaappdev@users.noreply.github.com>
2023-01-31 14:43:08 +0800

    reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = '';
+    for(let i = string.length -1; i >= 0; i--){
+        reverseString += string[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
