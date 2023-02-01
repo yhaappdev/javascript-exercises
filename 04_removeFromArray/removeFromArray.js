5741139819d8898666c62f4b22d98d6515bd5336
yhaappdev <121143197+yhaappdev@users.noreply.github.com>
2023-02-01 15:51:02 +0800

    remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toRemove, ...moreArgs) {
+    const index = array.indexOf(toRemove);
+    if(index > -1){
+        array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
