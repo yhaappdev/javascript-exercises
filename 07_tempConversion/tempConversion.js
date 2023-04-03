42163ab990116818546e254d82457f13b5589424
yhaappdev <121143197+yhaappdev@users.noreply.github.com>
2023-04-03 11:26:49 +0800

    exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const value = (temp - 273.15);
+  return Math.round(value * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const value = (temp + 273.15);
+  return Math.round(value * 10) / 10;
 };
 
 // Do not edit below this line
