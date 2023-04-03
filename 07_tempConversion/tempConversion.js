9488f310b5206bb0cd1f062f55e7062974d53722
yhaappdev <121143197+yhaappdev@users.noreply.github.com>
2023-04-03 11:50:05 +0800

    exercise 9

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,9 @@
-const convertToCelsius = function(temp) {
-  const value = (temp - 273.15);
-  return Math.round(value * 10) / 10;
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * (5 / 9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function(temp) {
-  const value = (temp + 273.15);
-  return Math.round(value * 10) / 10;
+const convertToFahrenheit = function(celcius) {
+  return Math.round(((celcius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[^A-Z]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
